fb2979135ab3cf6b362b6680d8a4a2135b2946e5
spammy <spammy7147@gmail.com>
2021-04-15 22:43:14 +0900

    mybatis mapper 작동확인 INSERT 작동확인

--- src/test/java/com/monfamily/wow/user/UserTestController.java
+++ src/test/java/com/monfamily/wow/user/UserTestController.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.util.Date;
+
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class UserTestController {
@@ -21,13 +23,14 @@ public class UserTestController {
     public void userAddTest(){
         System.out.println("userAddTest 메서드 실행");
         UserVO user = new UserVO();
-        user.setUserId("qwer12");
+        user.setUserId("qwer123");
         user.setUserPw("123");
         user.setUserName("홍길동");
         user.setUserMail("qwer12@gmail.com");
         user.setUserContact(1012345678);
+
         System.out.println("유저 : "+user);
-        mapper.userAdd(user);
+       mapper.userAdd(user);
     }
 
 }
